6e755614404b7e787bcaaf736fbff8cbdf6164c7
Felix Ude <felix.ude@aboutyou.com>
2021-09-02 22:22:05 +0200

    include cleanup in scrape script

--- scrape_data.py
+++ scrape_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 from google.cloud import bigquery
 import pytz
+import datetime
 
 # Construct a BigQuery client object.
 client = bigquery.Client()
@@ -12,13 +13,14 @@ table_id = "hamtraffic.all_data.iot_values"
 job_config = bigquery.LoadJobConfig(
     # Specify a (partial) schema. All columns are always written to the
     # table. The schema is used to assist in data type definitions.
-     schema=[
-    #    # Specify the type of columns whose type cannot be auto-detected. For
-    #    # example the "title" column uses pandas dtype "object", so its
-    #    # data type is ambiguous.
-         #bigquery.SchemaField("@iot.id", bigquery.enums.SqlTypeNames.STRING)
-         bigquery.SchemaField("phenomenonTime", bigquery.enums.SqlTypeNames.STRING)
-         bigquery.SchemaField("resultTime", bigquery.enums.SqlTypeNames.STRING)
+    schema=[
+        #    # Specify the type of columns whose type cannot be auto-detected. For
+        #    # example the "title" column uses pandas dtype "object", so its
+        #    # data type is ambiguous.
+        bigquery.SchemaField("iot_id", bigquery.enums.SqlTypeNames.STRING),
+        # bigquery.SchemaField("result", bigquery.enums.SqlTypeNames.FLOAT64),
+        # bigquery.SchemaField("resultTime", bigquery.enums.SqlTypeNames.STRING),
+    ],
     #    bigquery.SchemaField("title", bigquery.enums.SqlTypeNames.STRING),
     #    # Indexes are written if included in the schema by name.
     #    bigquery.SchemaField("wikidata_id", bigquery.enums.SqlTypeNames.STRING),
@@ -34,26 +36,31 @@ data = pd.DataFrame()
 
 url = "https://iot.hamburg.de/v1.1/Observations?$skip={}"
 
-for i in tqdm(range(0, 1000, 100)):
-    response = urlopen(url.format(i))
-    df_slice = pd.DataFrame(json.loads(response.read())["value"])
-    data = data.append(
-        df_slice.filter(
-            items=[
-                #"@iot.id",
-                "phenomenonTime",
-                "result",
-                "resultTime",
-            ]
+for i in tqdm(range(0, 1010000, 100)):
+    try:
+        response = urlopen(url.format(i))
+        df_slice = pd.DataFrame(json.loads(response.read())["value"])
+        data = data.append(
+            df_slice.filter(
+                items=[
+                    "@iot.id",
+                    "result",
+                    "resultTime",
+                ]
+            )
         )
-    )
-    if i % 10000 == 0:
-        pass
+        if i % 10000 == 0:
+            data["iot_id"] = data["@iot.id"].astype(str)
+            data.drop("@iot.id", axis=1, inplace=True)
+            data["resultTime"] = pd.to_datetime(data["resultTime"])
+            data["result"] = pd.to_numeric(data["result"], errors="coerce")
 
-job = client.load_table_from_dataframe(
-    data,
-    table_id,
-    job_config=job_config,
-)
-job.result()
-data = pd.DataFrame()
+            job = client.load_table_from_dataframe(
+                data,
+                table_id,
+                job_config=job_config,
+            )
+            job.result()
+            data = pd.DataFrame()
+    except:
+        raise Exception("URL could not be found")
