bd67fe74977b30b700158d704c1082f48f9b5ee7
Felix <felix@ude-consult.de>
2021-09-13 23:05:38 +0200

    working beam pipeline locally

--- traffic_beam.py
+++ traffic_beam.py
@@ -4,24 +4,21 @@ import json
 import logging
 from urllib.request import urlopen
 import pandas as pd
+from datetime import date, timedelta
 
-
-table_spec = "hamtraffic.all_data.car_traffic"
+yesterday = date.today() - timedelta(days=1)
+yesterday = yesterday.strftime("%Y-%m-%d")
+table_spec = "hamtraffic:all_data.car_traffic_daily_updates"
 base_url_cars = "https://iot.hamburg.de/v1.1/Things?$skip=0&$top=5000&$filter=((properties%2Ftopic+eq+%27Transport+und+Verkehr%27)+and+(properties%2FownerThing+eq+%27Freie+und+Hansestadt+Hamburg%27))"
 
 
 # beam_options = PipelineOptions()
-p = beam.Pipeline()
 
 
-class get_stations(beam.DoFn):
-    def __init__(self):
-        logging.debug("fetching stations")
-
-    def process(self, date):
-        logging.debug(f"Now fetching data for {date}")
-        response = json.loads(urlopen(base_url_cars).read())
-        return response["value"]
+def get_stations():
+    logging.debug(f"Now fetching all stations data.")
+    response = json.loads(urlopen(base_url_cars).read())
+    return response["value"]
 
 
 class get_station_urls(beam.DoFn):
@@ -63,29 +60,44 @@ class get_obs_stream(beam.DoFn):
                 }
             ]
         except KeyError:
-            logging.error("No proper observation stream found.")
+            logging.error(
+                f"No proper observation stream for {element['thingID']=} found."
+            )
 
 
 class get_obs(beam.DoFn):
-    def process(self, element):
+    def process(self, element, date):
         logging.debug(f"Now fetching observations for {element['thingID']}")
-        obs = json.loads(urlopen(element["obs_stream"]).read())["value"]
 
-        return [
-            {
-                "thingID": element["thingID"],
-                "description": element["description"],
-                "observedAreaCoordinates": element["observedAreaCoordinates"],
-                "obs": obs,
-            }
-        ]
+        obs_url = (
+            element["obs_stream"]
+            + "?$top=5000&$skip={}&$filter=date(phenomenontime)+eq+date('{}')"
+        )
+        list_of_obs = []
+        for i in range(0, 10000000, 5000):
+            obs_iter = json.loads(urlopen(obs_url.format(i, date)).read())["value"]
+            if len(obs_iter) == 0:
+                break
+            list_of_obs = list_of_obs + obs_iter
+
+        for obs in list_of_obs:
+            obs.update(
+                {
+                    "thingID": element["thingID"],
+                    "description": element["description"],
+                    "observedAreaCoordinates": element["observedAreaCoordinates"],
+                }
+            )
+
+        return list_of_obs
 
 
-class split_obs(beam.DoFn):
+class clean_obs(beam.DoFn):
     def process(self, element):
-        logging.debug(f"Now splitting observations for {element['thingID']}")
-        result = element["obs"]["result"]
-        resultTime = element["obs"]["resultTime"]
+        logging.debug(f"Now cleaning observations for {element['thingID']}")
+        observationID = element["@iot.id"]
+        resultTime = element["resultTime"]
+        result = element["result"]
 
         return [
             {
@@ -94,45 +106,50 @@ class split_obs(beam.DoFn):
                 "observedAreaCoordinates": element["observedAreaCoordinates"],
                 "result": result,
                 "resultTime": resultTime,
+                "observationID": observationID,
             }
         ]
 
 
-ingest_data = (
-    p
-    | "pass date" >> beam.Create(["2021-09-11"])
-    | "fetch stations data" >> beam.ParDo(get_stations())
-    | "get station urls" >> beam.ParDo(get_station_urls())
-    | "get obs stream" >> beam.ParDo(get_obs_stream())
-    | "get obs" >> beam.ParDo(get_obs())
-    | "split obs" >> beam.ParDo(split_obs())
-    # | 'write into gbq' >> beam.io.gcp.bigquery.WriteToBigQuery(table = table_spec, schema=table_schema, write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE ,create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
-    | "write to text" >> beam.io.WriteToText("./test_v2.csv")
-)
-
-
-"""
-quotes = p | beam.Create([
-    {
-        'source': 'Mahatma Gandhi', 'quote': 'My life is my message.'
-    },
-    {
-        'source': 'Yoda', 'quote': "Do, or do not. There is no 'try'."
-    },
-])
-
 table_schema = {
-    'fields': [{
-        'name': 'source', 'type': 'STRING', 'mode': 'NULLABLE'
-    }, {
-        'name': 'quote', 'type': 'STRING', 'mode': 'REQUIRED'
-    }]
+    "fields": [
+        {"name": "thingID", "type": "INT64"},
+        {"name": "description", "type": "STRING"},
+        {"name": "observedAreaCoordinates", "type": "STRING"},
+        {"name": "result", "type": "FLOAT64"},
+        {"name": "observationID", "type": "INT64"},
+        {"name": "resultTime", "type": "TIMESTAMP"},
+    ]
 }
 
-quotes | beam.io.WriteToBigQuery(
-    table_spec,
-    schema=table_schema,
-    custom_gcs_temp_location="gs://hamtraf_bucket",
-    write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
-    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
-    """
+
+def run():
+    p = beam.Pipeline()
+
+    all_stations = get_stations()
+
+    ingest_data = (
+        p
+        | "pass stations" >> beam.Create(all_stations)
+        | "get station urls" >> beam.ParDo(get_station_urls())
+        | "get obs stream" >> beam.ParDo(get_obs_stream())
+        | "get obs"
+        >> beam.ParDo(
+            get_obs(), yesterday
+        )  # BASH: date -d "yesterday 13:00" '+%Y-%m-%d'
+        | "clean obs" >> beam.ParDo(clean_obs())
+        | "write into gbq"
+        >> beam.io.gcp.bigquery.WriteToBigQuery(
+            table=table_spec,
+            schema=table_schema,
+            custom_gcs_temp_location="gs://hamtraf_bucket",
+            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+        )
+    )
+    result = p.run()
+    result.wait_until_finish()
+
+
+if __name__ == "__main__":
+    run()
